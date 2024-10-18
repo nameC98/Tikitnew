f32e0542d3c476150683ab94e72a8ab9a892f62e
CalvinDesigns <rapmastercalz@gmail.com>
2024-10-18 12:06:04 +0200

    spush

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,28 +6,57 @@ import axios from "axios";
 
 const Result = () => {
   const [order, setOrder] = useState();
+  const [ticketSent, setTicketSent] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const { orderUid } = useParams();
-
-  console.log(orderUid);
 
   useEffect(() => {
     const fetchOrder = async () => {
       try {
+        console.log("Fetching order with UID:", orderUid);
         const response = await axios.get(
           `https://api.tikiti.co.zw/opn/v1/orders/${orderUid}`
         );
-
+        console.log("Order fetched:", response.data);
         setOrder(response.data);
+
+        if (response.data.paymentTransaction.transactionStatus === "SUCCESS") {
+          console.log(
+            "Transaction is successful, proceeding to send tickets..."
+          );
+          await sendTickets();
+        } else {
+          console.log(
+            "Transaction status is not successful:",
+            response.data.paymentTransaction.transactionStatus
+          );
+        }
       } catch (error) {
         console.error("Error fetching order:", error);
+        setError("Error fetching order details. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const sendTickets = async () => {
+      try {
+        const response = await axios.post(
+          `https://api.tikiti.co.zw/opn/v1/orders/${orderUid}/send-tickets`
+        );
+        setTicketSent(true);
+        console.log("Tickets sent successfully:", response.data);
+      } catch (error) {
+        console.error("Error sending tickets:", error);
+        setError("Failed to send tickets. Please try again.");
       }
     };
 
     fetchOrder();
   }, [orderUid]);
-
-  console.log(order);
+  // console.log(order);
 
   return (
     <>
@@ -73,7 +102,11 @@ const Result = () => {
               </span>
             </div>
           </div>
-
+          {ticketSent && (
+            <div className="mt-4 text-green-600 text-center">
+              Tickets sent successfully to {order?.email}
+            </div>
+          )}
           <Link to="/">
             <div className="flex justify-center mt-8 mb-8">
               <button className="bg-orange-600  text-white font-semibold py-2 px-[7rem] rounded-md hover:bg-orange-500 transition-all duration-300">
